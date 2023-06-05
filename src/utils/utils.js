82d7bcf1b57d4edd67374ef5ff8ef8e660dddfa5
sanyaman <119109046+sanyaman@users.noreply.github.com>
2023-06-05 15:21:40 +0300

    Delete src directory

--- src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export default function renderLoading(loading, popup) {
-  if (loading) {
-    popup.querySelector(".popup__submit").textContent = "Сохранить";
-  } else {
-    popup.querySelector(".popup__submit").textContent = "Сохранение...";
-  }
-}
