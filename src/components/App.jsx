713cb4c9d3cdd90d9b2beb52fddf5178b1a96db5
sanyaman <119109046+sanyaman@users.noreply.github.com>
2023-06-06 21:16:06 +0300

    pr12afterRev1(fix)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
-//import "../index.css";
 import { api } from "../utils/Api";
 import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
 import * as auth from "../utils/auth";
